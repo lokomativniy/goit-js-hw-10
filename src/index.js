baa08f5e8d17ad82cc770bb89c340bd152e5ac0d
Dmitriy <dmitriy.sergiyenko.87@gmail.com>
2021-11-26 21:13:34 +0300

    hw10

--- src/index.js
+++ src/index.js
@@ -19,44 +19,25 @@ function onSearch(e) {
   if (name) {
     countries.fetchCountries(name).then(renderMarkap).catch(error);
   }
-  clearRenderMarkap()
+  clearRenderMarkap();
 }
+
 function clearRenderMarkap() {
   countryList.innerHTML = '';
   countryInfo.innerHTML = '';
 }
 
-
-
 function renderMarkap(country) {
-  if (country.length > 10){
-    Notiflix.Notify.info('Too many matches found. Please enter a more specific name.')
+  if (country.length > 10) {
+    Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
   }
   if (country.length <= 10 && country.length >= 2) {
     countryList.innerHTML = listOfCountries(country);
   }
-  if (country.length === 1) { 
-     countryInfo.innerHTML = infoOfCountry(country);
-  } 
+  if (country.length === 1) {
+    countryInfo.innerHTML = infoOfCountry(country);
+  }
 }
-
-// function listOfCountries(countries) {
-//   const markupTwoPlus = countries.map(({ name, capital, population, flags, languages }) => {
-//             return `<li><img src="${flags.svg}" alt="Flag of ${name.official}" style="height: 1em; width: 1em"> ${name.official}</li>`
-//         }).join('');
-//       countryEl.innerHTML = listOfCountries;
-// };
-
-// function markupOne(countries) {
-//       countries.map(({ name, capital, population, flags, languages }) => {
-//           const markup =
-//           `<h1><img src="${flags.svg}" alt="Flag of ${name.official}" style="height: 1em; width: 1em"> ${name.official}</h1>
-//       <li>Capital: ${capital}</li>
-//       <li>Population: ${population}</li>
-//       <li>Languages: ${Object.values(languages)}</li>`;
-//           infoEl.innerHTML = markup;
-//       })}
-
 
 function error() {
   return Notiflix.Notify.failure('Oops, there is no country with that name');
